ea7e9624336a01a3409931fe866a90c90bfd9371
Sivabalanmca <sivabalan.s@technoduce.com>
2016-05-20 14:18:28 +0530

    Yii Backend Generated - 20/05/16

---
+++ api/config/main-local.php
@@ -0,0 +1,12 @@
+<?php
+
+$config = [
+    'components' => [
+        'request' => [
+            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
+            'cookieValidationKey' => 'UVqZFVGj_2DRlxtu0haIgsLZM2W5KPg4',
+        ],
+    ],
+];
+
+return $config;
---
+++ api/modules/v1/controllers/DefaultController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace api\modules\v1\controllers;
+
+use yii\web\Controller;
+use Yii;
+
+/**
+ * Default controller for the `v1` module
+ */
+class DefaultController extends Controller
+{
+    /**
+     * Renders the index view for the module
+     * @return string
+     */
+    public function actionIndex()
+    {
+    	Yii::$app->language 	=	"en";
+    	echo Yii::t("api", "testing");
+    	//die("ssss");
+        //return $this->render('index');
+    }
+}
