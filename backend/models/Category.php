3578375f2b1ccf928871269aa9741c1f7efe8567
Sivabalanmca <sivabalan.s@technoduce.com>
2016-06-21 17:22:37 +0530

    Default Category layout changed - 21/06/16

--- backend/models/Category.php
+++ backend/models/Category.php
@@ -152,23 +152,26 @@ class Category extends \common\models\Category
     */
     public function getCategoryView($category_id = NULL)
     {
-        $this->categories .= "<ul>";
+        //$this->categories .= "<ul>";
         $expression =   new \yii\db\Expression('IFNULL(parent_category_id, 0) AS parent_category_id');
         $categories = self::find()->select(['category_id', 'category_name_en', 'category_name_ar', $expression])->where(['parent_category_id' => $category_id])->asArray()->all();
         foreach($categories AS $category)
         {
-            $this->categories .= "<li class='" . (($category_id) ? $category_id : 'base'). "'>"  . $category['category_name_en'] . ' ' . $category['category_name_ar'];
+            /*$this->categories .= "<td class='" . (($category_id) ? $category_id : 'base'). "'>"  . $category['category_name_en'] . ' ' . $category['category_name_ar'];*/
+            $this->categories .= "<tr data-id='" . $category['category_id'] . "' data-parent='" . (($category_id) ? $category_id : '') . "'>";
+            $this->categories .= "<td>" . $category['category_name_en'] . "</td>";
+            $this->categories .= "<td>" . $category['category_name_ar'] . "</td>";
             $vendorCount        =   self::find()->select(['category_id', 'category_name_en', 'category_name_ar', $expression])->join('JOIN', '{{%vendor_category_link}} AS vl', '{{%category}}.category_id = vl.category_id')->where(['{{%category}}.category_id' => $category['category_id']])->count();
-            $this->categories .= "<div class='pull-right'>";
+            $this->categories .= "<td>";
             if(!$vendorCount)
                 $this->categories .= '<a class="add_category" data-attribute-id="' . $category['category_id'] . '" data-attribute-name="' . $category['category_name_en'] . '"><i class="fa fa-plus fa-fw"></i></a>';
-            $this->categories .= "<a class='view' id='" . $category['category_id'] . "' href='#'><i class='fa fa-eye fa-fw'></i></a><a id='" . $category['category_id'] . "' class='update' href='#'><i class='fa fa-edit fa-fw'></i></a><a class='actions' onclick='return confirm(\"Are you sure want to delete?\")' href='" . Url::to(['defaultcategory/delete', 'id' => $category['category_id']]) . "'><i class='fa fa-trash fa-fw fa-1x'></i></a></div>";
-
+            $this->categories .= "<a class='view' id='" . $category['category_id'] . "' href='#'><i class='fa fa-eye fa-fw'></i></a><a id='" . $category['category_id'] . "' class='update' href='#'><i class='fa fa-edit fa-fw'></i></a><a class='actions' onclick='return confirm(\"Are you sure want to delete?\")' href='" . Url::to(['defaultcategory/delete', 'id' => $category['category_id']]) . "'><i class='fa fa-trash fa-fw fa-1x'></i></a></td>";
+            $this->categories .= "</tr>";
             if($category)
                 $this->getCategoryView($category['category_id']);
-            $this->categories .= "</li>";
+            //$this->categories .= "</li>";
         }
-        $this->categories .= "</ul>";
+        //$this->categories .= "</tr>";
         return $this->categories;
     }
 }
